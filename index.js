03d4ae3dee5caee98353da08c31c91d04a405470
Mosae Litsoane <mosaelitsoane@Mosaes-MacBook-Pro.local>
2020-02-26 10:46:20 -0600

    6 solved

--- index.js
+++ index.js
@@ -595,8 +595,11 @@ function processProduct(num1, num2, callback) {
  * "lady gaga" and `['foo', 'bar']` and `(bool) => bool ? 'nice!' : 'sad'`,
  * should return "sad".
  */
-function processContains( /* CODE HERE */ ) {
+function processContains(item, list, callback) {
   /* CODE HERE */
+  var present = list.indexOf(item) > -1;
+  return callback(present)
+
 }
 
 /**
